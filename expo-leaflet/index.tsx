fa2d55f3f612b00b068052243ecba6ca996e5ad7
damien <damien@geev.com>
2021-09-24 15:20:51 +0200

    Update version

--- expo-leaflet/index.tsx
+++
@@ -1,9 +0,0 @@
-export { ExpoLeaflet } from './ExpoLeaflet'
-export { ExpoLeafletProps } from './web/src/ExpoLeaflet.types'
-export {
-  LeafletWebViewEvent,
-  MapMarker,
-  MapLayer,
-  MapLayerType,
-  MapShape,
-} from './web/src/model'
